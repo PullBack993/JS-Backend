c464c400aaa4100e6c4e6f0149f75fb612d6c2a2
Pullback993 <turgay.durhanov.ismailov@gmail.com>
2022-05-02 21:43:33 +0200

    add

---
+++ Exam-Preparation-Part-2/config/database.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+require('../models/User');
+require('../models/Trip');
+
+const dbName = 'trip';
+const connectionString = `mongodb://localhost:27017/${dbName}`;
+
+module.exports = async (app) => {
+    try {
+        await mongoose.connect(connectionString, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+
+        console.log('Database connection');
+        
+        mongoose.connection.on('error', (err) => {
+            console.error('Database error');
+            console.error(err);
+        })
+    } catch (err) {
+        console.error('Error connection to database');
+        process.exit(1);
+    }
+};
+
