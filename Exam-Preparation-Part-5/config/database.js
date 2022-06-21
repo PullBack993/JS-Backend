a293efa213a586a7676c3187c49b512deb77a193
Pullback993 <turgay.durhanov.ismailov@gmail.com>
2022-06-21 10:34:58 +0200

    add

---
+++ Exam-Preparation-Part-5/config/database.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+require('../models/Publication');
+require('../models/User');
+
+const dbName = 'artGalary';
+
+const connectionString = `mongodb://localhost:27017/${dbName}`;
+
+module.exports = async (app) => {
+    try {
+        await mongoose.connect(connectionString, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log('Database connection');
+        
+        mongoose.connection.on('error', (err) => {
+            console.error('Database error');
+            console.error(err);
+        })
+    } catch (err) {
+        console.error('Error connection to database');
+        process.exit(1);
+    }
+};
